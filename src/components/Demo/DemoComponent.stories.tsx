04ffd1e8423013e30dd752bbbbb88657a9665b33
Sebastian Key <sebastianwkey@gmail.com>
2021-09-06 15:39:57 -0700

    remove demo component

--- src/components/Demo/DemoComponent.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { DemoComponent } from "./DemoComponent";
-
-export default {
-  title: "Demo",
-  component: DemoComponent,
-};
-
-export const DemoOne = () => <DemoComponent />;
